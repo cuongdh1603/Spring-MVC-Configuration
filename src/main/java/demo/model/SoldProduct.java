30746005bb76da3444e08f9b7a317bf7dee17930
DESKTOP-1HJAFC2\thanh <thanhnt21092001@gmail.com>
2023-11-03 18:00:45 +0700

    commit

--- src/main/java/demo/model/SoldProduct.java
+++ src/main/java/demo/model/SoldProduct.java
@@ -2,69 +2,69 @@ package demo.model;
 
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "SOLDPRODUCT")
 public class SoldProduct {
-	
-	@Id
-	@Column(name = "ID")
-	private String id;
-	
-	@Column(name = "quantity")
-	private Integer quantity;
-//	
-	@OneToMany(mappedBy = "SOLDPRODUCT")
-	private List<Bill> bills;
-	
-	@ManyToOne
-	@JoinColumn(name = "branch_id")
-	private Branch branch;
-	
-	@ManyToOne
-	@JoinColumn(name = "product_id")
-	private Product product;
-	
-	
 
-	public String getId() {
-		return id;
-	}
+    @Id
+    @Column(name = "ID")
+    private String id;
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    @Column(name = "quantity")
+    private Integer quantity;
 
-	public Integer getQuantity() {
-		return quantity;
-	}
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "soldProduct", cascade = CascadeType.ALL)
+    private List<Bill> bills;
 
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
-	}
+    @ManyToOne
+    @JoinColumn(name = "branch_id")
+    private Branch branch;
 
-	public Branch getBranch() {
-		return branch;
-	}
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 
-	public void setBranch(Branch branch) {
-		this.branch = branch;
-	}
 
-	public Product getProduct() {
-		return product;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setProduct(Product product) {
-		this.product = product;
-	}
-	
-	
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Branch getBranch() {
+        return branch;
+    }
+
+    public void setBranch(Branch branch) {
+        this.branch = branch;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public List<Bill> getBills() {
+        return bills;
+    }
+
+    public void setBills(List<Bill> bills) {
+        this.bills = bills;
+    }
+
 }
