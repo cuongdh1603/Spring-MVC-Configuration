e5d3f1d4d2de39119d89330b17efee11922e3de5
cuongdh1603 <dohungcuong16032001@gmail.com>
2023-11-03 22:49:03 +0700

    commit new

--- src/main/java/demo/repository/BillRepository.java
+++ src/main/java/demo/repository/BillRepository.java
@@ -1,8 +1,6 @@
 package demo.repository;
 
-import demo.model.Bill;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BillRepository extends JpaRepository<Bill,Integer> {
-
+public interface BillRepository{
+	
 }
--- src/main/java/demo/serviceImpl/BillServiceImpl.java
+++ src/main/java/demo/serviceImpl/BillServiceImpl.java
@@ -1,13 +1,10 @@
 package demo.serviceImpl;
 
-import demo.repository.BillRepository;
 import demo.service.BillService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BillServiceImpl implements BillService {
-    @Autowired
-    private BillRepository repo;
+    
 
 }
